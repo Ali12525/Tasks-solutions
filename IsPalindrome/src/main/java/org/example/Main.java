40c00a72e04492e65ec595ed5c7b840ad93604f8
Ali12525 <133050841+Ali12525@users.noreply.github.com>
2025-08-16 23:14:29 +0300

    IsPalindrome

---
+++ IsPalindrome/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+
+    public static boolean isPalindrome(String text) {
+        String cleaned = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        return cleaned.equals(new StringBuilder(cleaned).reverse().toString());
+    }
+}
